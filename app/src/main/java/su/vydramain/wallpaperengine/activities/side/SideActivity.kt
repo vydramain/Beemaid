c6e717ab42483a3a2cabd6ba574341eb36ac2477
vydra <vydramain@gmail.com>
2023-06-09 03:01:24 +0600

    feat: Creating layout of Jetpack Compose

--- app/src/main/java/su/vydramain/wallpaperengine/activities/side/SideActivity.kt
+++ app/src/main/java/su/vydramain/wallpaperengine/activities/side/SideActivity.kt
@@ -3,7 +3,24 @@ package su.vydramain.wallpaperengine.activities.side
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import su.vydramain.wallpaperengine.R
+
 
 class SideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,4 +30,38 @@ class SideActivity : AppCompatActivity() {
             Text("Fuck you!")
         }
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(inputMessage: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.nanashi_mumei),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(inputMessage.author)
+            // Add a vertical space between the author and message text
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(inputMessage.body)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(Message("Nanashi Mumei", "Oh, hi"))
 }
