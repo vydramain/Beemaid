86200a7e19d7b325bc3d37c4c575c3422515df26
vydra <vydramain@gmail.com>
2023-06-12 17:00:54 +0600

    feat: Implemented material design

--- app/src/main/java/su/vydramain/wallpaperengine/activities/side/SideActivity.kt
+++ app/src/main/java/su/vydramain/wallpaperengine/activities/side/SideActivity.kt
@@ -1,17 +1,22 @@
 package su.vydramain.wallpaperengine.activities.side
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import su.vydramain.wallpaperengine.R
+import su.vydramain.wallpaperengine.ui.theme.WallpaperEngineTheme
 
 
 class SideActivity : AppCompatActivity() {
@@ -27,7 +33,11 @@ class SideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text("Fuck you!")
+            WallpaperEngineTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Nanashi Mumei", "Oh, hi"))
+                }
+            }
         }
     }
 }
@@ -43,25 +53,45 @@ fun MessageCard(inputMessage: Message) {
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 // Set image size 40 dp
-                .size(40.dp)
+                .size(60.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
 
         // Add a horizontal space between the image and column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(inputMessage.author)
+            Text(
+                text = inputMessage.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
             // Add a vertical space between the author and message text
             Spacer(modifier = Modifier.height(4.dp))
-            Text(inputMessage.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = inputMessage.body,
+                    modifier = Modifier.padding(all = 7.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode")
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Nanashi Mumei", "Oh, hi"))
+    WallpaperEngineTheme {
+        Surface {
+            MessageCard(inputMessage = Message("Nanashi Mumei", "Oh, hi"))
+        }
+    }
 }
