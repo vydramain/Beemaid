0ce3ad0d45c3c10435149534f6268f1c1895f0d3
vydra <vydramain@gmail.com>
2023-06-16 23:18:02 +0600

    feat: Added row

--- app/src/main/java/su/vydramain/wallpaperengine/ui/components/WallpaperPreView.kt
+++ app/src/main/java/su/vydramain/wallpaperengine/ui/components/WallpaperPreView.kt
@@ -1,26 +1,34 @@
 package su.vydramain.wallpaperengine.ui.components
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
 import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 import su.vydramain.wallpaperengine.R
 import su.vydramain.wallpaperengine.data.Wallpaper
 
+@SuppressLint("UseCompatLoadingForDrawables")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WallpaperPreView(
@@ -59,6 +67,37 @@ fun WallpaperPreView(
                         modifier = Modifier
                             .fillMaxWidth()
                     )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = resources.getString(R.string.wallpaper_settings_duration_title),
+                            color = MaterialTheme.colorScheme.secondary,
+                            style = MaterialTheme.typography.titleSmall,
+                            modifier = Modifier
+                                .width(70.dp)
+                        )
+
+                        TextField(
+                            value = wallpaper.path,
+                            onValueChange = { wallpaper.path = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+
+                        Image(
+                            painter = painterResource(R.drawable.add),
+                            contentDescription = resources.getString(R.string.wallpaper_settings_duration_button_add_description),
+                            modifier = Modifier.fillMaxHeight()
+                        )
+
+                        Image(
+                            painter = painterResource(R.drawable.remove),
+                            contentDescription = resources.getString(R.string.wallpaper_settings_duration_button_reduce_description),
+                            modifier = Modifier.fillMaxHeight()
+                        )
+                    }
                 }
             }
         }
