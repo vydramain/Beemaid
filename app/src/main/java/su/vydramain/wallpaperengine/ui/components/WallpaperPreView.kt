537a87e21f696e02217c70060bd4c1bb02917bf1
vydra <vydramain@gmail.com>
2023-06-17 22:55:31 +0600

    feat: Created compose form by chatGPT

--- app/src/main/java/su/vydramain/wallpaperengine/ui/components/WallpaperPreView.kt
+++ app/src/main/java/su/vydramain/wallpaperengine/ui/components/WallpaperPreView.kt
@@ -1,28 +1,28 @@
 package su.vydramain.wallpaperengine.ui.components
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.res.Resources
 import android.net.Uri
+import android.widget.ImageButton
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 
 import su.vydramain.wallpaperengine.R
@@ -34,72 +34,120 @@ import su.vydramain.wallpaperengine.data.Wallpaper
 fun WallpaperPreView(
     wallpaper: Wallpaper
 ) {
-    val resources: Resources = Resources.getSystem()
-
     val standardPadding = 16.dp
 
-    BoxWithConstraints {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = standardPadding, end = standardPadding, top = standardPadding)
-        )
-        {
-            Row(
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+                    .weight(1f)
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = standardPadding)
-                ) {
+                Text(
+                    text = stringResource(R.string.wallpaper_settings_title),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done
+                    )
+                )
+
+                Row(modifier = Modifier.fillMaxWidth()) {
                     Text(
-                        text = resources.getString(R.string.wallpaper_settings_title),
-                        color = MaterialTheme.colorScheme.secondary,
-                        style = MaterialTheme.typography.titleSmall,
-                        modifier = Modifier
-                            .fillMaxWidth()
+                        text = stringResource(R.string.wallpaper_settings_duration_title),
+                        modifier = Modifier.width(70.dp)
                     )
+
                     TextField(
-                        value = wallpaper.path,
-                        onValueChange = { wallpaper.path = it },
-                        modifier = Modifier
-                            .fillMaxWidth()
+                        value = "",
+                        onValueChange = {},
+                        modifier = Modifier.weight(1f),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        )
                     )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Text(
-                            text = resources.getString(R.string.wallpaper_settings_duration_title),
-                            color = MaterialTheme.colorScheme.secondary,
-                            style = MaterialTheme.typography.titleSmall,
-                            modifier = Modifier
-                                .width(70.dp)
-                        )
 
-                        TextField(
-                            value = wallpaper.path,
-                            onValueChange = { wallpaper.path = it },
-                            modifier = Modifier
-                                .fillMaxWidth()
+                    Row(modifier = Modifier.weight(1f)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.add),
+                            contentDescription = stringResource(R.string.wallpaper_settings_duration_button_add_description),
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier.weight(1f)
                         )
 
                         Image(
-                            painter = painterResource(R.drawable.add),
-                            contentDescription = resources.getString(R.string.wallpaper_settings_duration_button_add_description),
-                            modifier = Modifier.fillMaxHeight()
+                            painter = painterResource(id = R.drawable.add),
+                            contentDescription = stringResource(R.string.wallpaper_settings_duration_button_reduce_description),
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
+                }
+
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = stringResource(R.string.wallpaper_settings_transition_title),
+                        modifier = Modifier.width(70.dp)
+                    )
+
+                    TextField(
+                        value = "",
+                        onValueChange = {},
+                        modifier = Modifier.weight(1f),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        )
+                    )
+
+                    Row(modifier = Modifier.weight(1f)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.add),
+                            contentDescription = stringResource(R.string.wallpaper_settings_transition_button_add_description),
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier.weight(1f)
                         )
 
                         Image(
-                            painter = painterResource(R.drawable.remove),
-                            contentDescription = resources.getString(R.string.wallpaper_settings_duration_button_reduce_description),
-                            modifier = Modifier.fillMaxHeight()
+                            painter = painterResource(id = R.drawable.add),
+                            contentDescription = stringResource(R.string.wallpaper_settings_transition_button_reduce_description),
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier.weight(1f)
                         )
                     }
                 }
             }
+
+            Image(
+                painter = painterResource(R.drawable.ic_menu_gallery),
+                contentDescription = stringResource(R.string.wallpaper_preview_description),
+                modifier = Modifier
+                    .size(72.dp)
+                    .weight(1f)
+            )
+
+            // Other components can be added here for additional settings
+        }
+
+        Button(
+            onClick = { /* Handle button click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+        ) {
+            Text(text = stringResource(R.string.wallpaper_choose_button_text))
         }
     }
 }
